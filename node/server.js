08db27ecb129d08a1f64492b9859a4476f32f45b
Lash <laseclatom@gmail.com>
2016-07-28 02:20:33 -0700

    heroku is stress

--- node/server.js
+++ node/server.js
@@ -1,12 +1,12 @@
-// var express    = require("express");
- var mysql      = require('mysql');
- var connection = mysql.createConnection({
+var express    = require("express");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
    host     : 'cainafricadb.db.11956473.hostedresource.com',
    user     : 'cainafricadb',
    password : 'Cainwebdev@2016',
    database : 'cainafricadb'
  });
- // var app = express();
+var app = express();
  
 
-// app.listen(process.env.PORT);
+app.listen(process.env.PORT);
